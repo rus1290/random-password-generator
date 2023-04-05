7823b146ab5fdd0f8c5ad509939badfadbcfe930
Rus Zakirov <81056953+rus1290@users.noreply.github.com>
2023-04-05 16:20:28 -0400

    created function that will generate password.

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey! You clicked me! :)");
+
+  // The required code to make the function work:
+  // 1. Prompt the user for password length
+  // 2. Prompt the user for character type
+  // 3. Check wether the input matches the criteria
+  // 4. Generate password based on the criteria
+  // 5. Display password to the page
+
+  return "Test, test"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
