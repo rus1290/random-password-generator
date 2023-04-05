83d1b9109263e8fb7f2ce41909f4ac92d9e6e0e7
Rus Zakirov <81056953+rus1290@users.noreply.github.com>
2023-04-05 17:44:51 -0400

    added function that prompts the user for character type and stores their choice.

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// this function prompts the user to choose length of the password and also validates the answer of a user.
 function charLength() {
   var length = prompt("Please choose the number between 8 to 128 characters.");
     if (length >= 8 && length < 128 && !isNaN(length)){
@@ -12,6 +13,26 @@ function charLength() {
     alert("Please try agian!");
   }
   return length
+}
+
+// this function prompts the user for character type;
+// while loop will iterate through all character type choices and will store them in a variable choice.
+function charType() {
+  var choice = " ";
+  while(choice == " "){
+    var lower = confirm("Do you want to include lower characters?");
+    if (lower == true){choice += "l"};
+    console.log(choice);
+    var upper = confirm("Do you want to include upper characters?");
+    if (lower == true){choice += "u"};
+    console.log(choice);
+    var number = confirm("Do you want to include numbers?");
+    if (number == true){choice += "n"};
+    console.log(choice);
+    var special = confirm("Do you want to include special characters?");
+    if (special == true){choice += "s"};
+    console.log(choice);
+  }
 }
 
 function generatePassword() {
@@ -24,6 +45,7 @@ function generatePassword() {
   // 5. Display password to the page
   
   var passwordLength = charLength();
+  var charChoice = charType();
 }
 
 // Write password to the #password input
